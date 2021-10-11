a600066f43f948ec58ba0e80333a8c1278e9ea76
Exelsioro <pvlyoyo@gmail.com>
2021-10-11 19:21:25 +0300

    Created pages and components

--- CognizantTestUI/src/app/app.module.ts
+++ CognizantTestUI/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamePageComponent } from './pages/game-page/game-page.component';
+import { StatisticsPageComponent } from './pages/statistics-page/statistics-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskComponent } from './components/task/task.component';
+import { StatisticComponent } from './components/statistic/statistic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamePageComponent,
+    StatisticsPageComponent,
+    HeaderComponent,
+    TaskComponent,
+    StatisticComponent
   ],
   imports: [
     BrowserModule,
