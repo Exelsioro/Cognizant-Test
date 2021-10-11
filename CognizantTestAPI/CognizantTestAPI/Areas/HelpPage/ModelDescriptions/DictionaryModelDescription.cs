f46ab7fedde190a3960f126724a08b160293d131
Exelsioro <pvlyoyo@gmail.com>
2021-10-11 19:39:57 +0300

    Created API solution and nessesary projets

---
+++ CognizantTestAPI/CognizantTestAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CognizantTestAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CognizantTestAPI/CognizantTestAPI/Controllers/TaskController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace CognizantTestAPI.Controllers
+{
+    public class TaskController : ApiController
+    {
+
+        [HttpGet]
+        public object Get()
+        {
+            return null;
+        }
+        // GET api/values/5
+        [HttpGet]
+        public object Get(int id)
+        {
+            return null;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] object body)
+        {
+
+        }
+    }
+}
